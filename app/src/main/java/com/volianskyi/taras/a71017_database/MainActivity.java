b2da46c210dc2aca65960d46e70fcc16f3b4164f
TarasVolianskyi <volianskyitv@gmail.com>
2017-10-10 17:17:32 +0300

    Update functions

--- app/src/main/java/com/volianskyi/taras/a71017_database/MainActivity.java
+++ app/src/main/java/com/volianskyi/taras/a71017_database/MainActivity.java
@@ -1,5 +1,6 @@
 package com.volianskyi.taras.a71017_database;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -11,6 +12,7 @@ import android.widget.Adapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private SQLiteDatabase sqLiteDatabase;
@@ -23,9 +25,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button btnRead = (Button) findViewById(R.id.btnReaderMainActivity);
         Button btnAdd = (Button) findViewById(R.id.btnAddMainActivity);
         Button btnUpdateList = (Button) findViewById(R.id.btnUpdateListMainActivity);
+        Button btnGoToSecond = (Button) findViewById(R.id.btnGoToSecondMainActivity);
         btnRead.setOnClickListener(this);
         btnAdd.setOnClickListener(this);
         btnUpdateList.setOnClickListener(this);
+        btnGoToSecond.setOnClickListener(this);
 
     }
 
@@ -34,12 +38,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onStart();
         DBHelper dbHelper = new DBHelper(this, 1);
         sqLiteDatabase = dbHelper.getWritableDatabase();//read & write to db
-
+        listView = (ListView) findViewById(R.id.lvAllInfoMainActivity);
     }
 
     @Override
     public void onClick(View view) {
-        Intent intent;
+        Intent intent = null;
         switch (view.getId()) {
             case R.id.btnReaderMainActivity:
                 Cursor cursor = sqLiteDatabase.query("USERS", null, null, null, null, null, null);
@@ -53,17 +57,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 } while (cursor.moveToNext());
                 break;
             case R.id.btnAddMainActivity:
-                sqLiteDatabase.execSQL(String.format("INSERT INTO USERS (NAME, PASS) VALUES ('%s','%s')", "Roman", "qwerty"));
+                //sqLiteDatabase.execSQL(String.format("INSERT INTO USERS (NAME, PASS) VALUES ('%s','%s')", "Roman", "qwerty"));
+                ContentValues contentValues = new ContentValues();
+                contentValues.put("NAME", "Sergiy");
+                contentValues.put("PASS", "asdfg");
+                //Log.d("Database", "Insert - "+ sqLiteDatabase.insert("USERS", null, contentValues));
+                //sqLiteDatabase.insert("USERS", null, contentValues);
+                //sqLiteDatabase.update("USERS", contentValues, "_id = ?", new String[]{"1"});//for update
+                Log.d("Database", "Deleted - " + sqLiteDatabase.delete("USERS", null , null));
+                //sqLiteDatabase.delete("USERS","_id = ?", new String[]{"1"});
                 break;
             case R.id.btnUpdateListMainActivity:
                 Log.d("Database", "Cursor size is - " + 5);
-                intent = new Intent(this, SecondActivity.class);
-                startActivity(intent);
-                /*Cursor cursorList = sqLiteDatabase.query("USERS", null, null, null, null, null, null);
+                // intent = new Intent(MainActivity.this, SecondActivity.class);
+                //startActivity(intent);
+                Cursor cursorList = sqLiteDatabase.query("USERS", null, null, null, null, null, null);
                 SimpleCursorAdapter simpleCursorAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, cursorList, new String[]{"NAME"}, new int[]{android.R.id.text1}, Adapter.NO_SELECTION);
-                listView.setAdapter(simpleCursorAdapter);*/
+                listView.setAdapter(simpleCursorAdapter);
                 break;
+            case R.id.btnGoToSecondMainActivity:
+                Toast.makeText(this, "Click", Toast.LENGTH_SHORT).show();
+                intent = new Intent(this, SecondActivity.class);
+                intent.putExtra("Number", 15);
+                startActivity(intent);
         }
-
     }
 }
--- app/src/main/java/com/volianskyi/taras/a71017_database/SecondActivity.java
+++ app/src/main/java/com/volianskyi/taras/a71017_database/SecondActivity.java
@@ -7,28 +7,38 @@ import android.os.Bundle;
 import android.widget.Adapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
     private SQLiteDatabase sqLiteDatabase;
     private ListView listView;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-       // listView = (ListView) findViewById(R.id.lvAllInfoSecondActivity);
+        tv = (TextView) findViewById(R.id.tvHelloTextSecondActivity);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        DBHelper dbHelper = new DBHelper(this, 1);
+        sqLiteDatabase = dbHelper.getWritableDatabase();//read & write to db
+        listView = (ListView) findViewById(R.id.lvAllInfoSecondActivity);
+        initListView();
+    }
+
+    private void initListView() {
+        listView = (ListView) findViewById(R.id.lvAllInfoSecondActivity);
         Cursor cursorList = sqLiteDatabase.query("USERS", null, null, null, null, null, null);
         SimpleCursorAdapter simpleCursorAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, cursorList, new String[]{"NAME"}, new int[]{android.R.id.text1}, Adapter.NO_SELECTION);
         listView.setAdapter(simpleCursorAdapter);
     }
 
-    /*@Override
-    protected void onStart() {
-        super.onStart();
-        DBHelper dbHelper = new DBHelper(this, 1);
-        sqLiteDatabase = dbHelper.getWritableDatabase();//read & write to db
-
-    }*/
 
 }
